040a1db3626d49f42a6e76f9ca156aa9b8d703ac
bgibers <bgibers@clemson.edu>
2017-04-19 21:28:16 -0400

    First commit

---
+++ player.h
@@ -0,0 +1,33 @@
+#ifndef PLAYER__H
+#define PLAYER__H
+#include <string>
+#include <vector>
+#include "twoWaysprite.h"
+#include "sprite.h"
+//#include "clock.h"
+
+class Player : public TwoWaySprite
+{
+public:
+	Player();
+	Player(const std::string&);
+	Player(const Player& p);
+
+
+
+	virtual void update(Uint32 ticks);
+	void stop();
+	void left();
+	void right();
+	void up();
+	void down();
+	
+	protected:
+		Vector2f intitialVelocity;
+		const float slowDown;
+
+	private:
+	//	Clock& clock;
+};
+
+#endif
